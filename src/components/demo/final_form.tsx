8ba9f0bcf7df750e58212694809299c9f80bcffc
Juergen Gmeiner <spamless.juergen@gmail.com>
2023-03-16 20:30:31 +0100

    cleaned up final form example

--- src/components/demo/final_form.tsx
+++ src/components/demo/final_form.tsx
@@ -1,27 +1,56 @@
 // https://codesandbox.io/s/github/final-form/react-final-form/tree/master/examples/field-level-validation?file=/index.js:0-2707
 import { h, Fragment } from "preact"
 import { Form, Field, FieldRenderProps } from "react-final-form"
+import { useId } from "preact/hooks"
 
 export const onSubmitLog = (values: unknown) => {
   console.log("onSubmit", JSON.stringify(values, undefined, 2))
 }
 
 const required = (value: unknown) => (value ? undefined : "Required")
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const mustBeNumber = (value: any) =>
   isNaN(value) ? "Must be a number" : undefined
-const minValue = (min: any) => (value: any) =>
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const minValue = (min: number) => (value: any) =>
   isNaN(value) || value >= min ? undefined : `Should be greater than ${min}`
 const composeValidators =
-  (...validators: any[]) =>
-  (value: unknown) =>
-    validators.reduce(
-      (error, validator) => error || validator(value),
-      undefined,
-    )
+  (...validators: unknown[]) =>
+  (value: unknown) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const vs: any[] = validators
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const v: any = value
+    return vs.reduce((error, validator) => error || validator(v), undefined)
+  }
 
 type Props = {
-  // async submit is also possible, just return Promise<void>
-  onSubmit: (values: any) => void
+  onSubmit: (values: unknown) => void
+}
+
+const HInput = ({ input, meta, label }: FieldRenderProps<string>) => {
+  const id = useId()
+  return (
+    <div class="field is-horizontal">
+      <div class={`field-label is-normal`}>
+        <label class="label" htmlFor={id}>
+          {label}
+        </label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <div class="control">
+            <input id={id} class="input" {...input} />
+          </div>
+          {meta.error && meta.touched && (
+            <span role="alert" class="help is-danger">
+              {meta.error}
+            </span>
+          )}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export const FinalFormExample = (props: Props) => (
@@ -30,95 +59,50 @@ export const FinalFormExample = (props: Props) => (
       React Final Form Example
     </h1>
     <h2 class="subtitle">Synchronous Field-Level Validation</h2>
-    <a
-      href="https://final-form.org/react"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Read Docs
-    </a>
+
     <Form
       onSubmit={props.onSubmit}
       render={({ handleSubmit, form, submitting, pristine, values }) => (
         <form onSubmit={handleSubmit}>
-          <Field name="firstName" validate={required}>
-            {({ input, meta }: FieldRenderProps<string>) => (
-              <div class="field">
-                <label class="label">First Name</label>
-                <div class="control">
-                  <input
-                    {...input}
-                    class="input"
-                    type="text"
-                    placeholder="First Name"
-                  />
-                </div>
-                {meta.error && meta.touched && (
-                  <span role="alert" class="help is-danger">
-                    {meta.error}
-                  </span>
-                )}
-              </div>
-            )}
-          </Field>
-          <Field name="lastName" validate={required}>
-            {({ input, meta }: FieldRenderProps<string>) => (
-              <div class="field">
-                <label class="label">Last Name</label>
-                <div class="control">
-                  <input
-                    {...input}
-                    class="input"
-                    type="text"
-                    placeholder="Last Name"
-                  />
-                </div>
-                {meta.error && meta.touched && (
-                  <span role="alert" class="help is-danger">
-                    {meta.error}
-                  </span>
-                )}
-              </div>
-            )}
-          </Field>
+          <Field
+            name="firstName"
+            validate={required}
+            label="First Name"
+            render={HInput}
+          />
+          <Field
+            name="lastName"
+            validate={required}
+            label="Last Name"
+            render={HInput}
+          />
           <Field
             name="age"
             validate={composeValidators(required, mustBeNumber, minValue(18))}
-          >
-            {({ input, meta }: FieldRenderProps<string>) => (
+            label="Age"
+            render={HInput}
+          />
+          <div class="field is-horizontal">
+            <div class="field-label" />
+            <div class="field-body">
               <div class="field">
-                <label class="label">Age</label>
-                <div class="control">
-                  <input
-                    {...input}
-                    class="input"
-                    type="text"
-                    placeholder="Age"
-                  />
+                <div class="control is-grouped">
+                  <button type="submit" class="button" disabled={submitting}>
+                    Submit
+                  </button>
+                  <button
+                    type="button"
+                    class="button"
+                    onClick={form.reset}
+                    disabled={submitting || pristine}
+                  >
+                    Reset
+                  </button>
                 </div>
-                {meta.error && meta.touched && (
-                  <span role="alert" class="help is-danger">
-                    {meta.error}
-                  </span>
-                )}
               </div>
-            )}
-          </Field>
-          <div class="field">
-            <div class="control is-grouped">
-              <button type="submit" class="button" disabled={submitting}>
-                Submit
-              </button>
-              <button
-                type="button"
-                class="button"
-                onClick={form.reset}
-                disabled={submitting || pristine}
-              >
-                Reset
-              </button>
             </div>
           </div>
+
           <pre>{JSON.stringify(values, undefined, 2)}</pre>
         </form>
       )}
